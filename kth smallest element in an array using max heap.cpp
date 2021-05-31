df7d1bb382bb19b4ec767823c653c9565769f6f4
rayanubhab <59302068+rayanubhab@users.noreply.github.com>
2021-05-31 11:33:11 +0530

    New commit

---
+++ kth smallest element in an array using max heap.cpp
@@ -0,0 +1,17 @@
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+       
+      priority_queue<int> mxhp;
+      for(int i=l;i<=r;i++){
+      mxhp.push(arr[i]);
+      if(mxhp.size()>k){
+       mxhp.pop();}
+
+}
+     return mxhp.top();
+      
+         
+    }
