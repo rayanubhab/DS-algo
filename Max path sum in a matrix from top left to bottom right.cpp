6d9e111b955176aea3d50802a7927d9c81373d0b
rayanubhab <rayanubhab@gmail.com>
2022-05-28 10:28:16 +0530

    Asked in Amdocs OA[NITA]

---
+++ Max path sum in a matrix from top left to bottom right.cpp
@@ -0,0 +1,23 @@
+int solve(vector<vector<int>> &grid) {
+    int n = grid.size();
+    if(n == 0) return 0;
+    int m = grid[0].size();
+
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            if(i == 0 && j == 0){
+                continue;
+            }
+            else if(i == 0) {
+                grid[i][j] += grid[i][j-1];
+            }
+            else if(j == 0) {
+                grid[i][j] += grid[i-1][j];
+            }
+            else {
+                grid[i][j] += max(grid[i-1][j],grid[i][j-1]);
+            }
+        }
+    }
+    return grid[n-1][m-1];
+}
